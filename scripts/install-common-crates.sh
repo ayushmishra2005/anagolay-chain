72ee2742f495b244288dccba3d0be4c88d278d7d
Daniel Maricic <daniel@woss.io>
2022-07-04 11:19:34 +0000

    add missing crates

--- scripts/install-common-crates.sh
+++ scripts/install-common-crates.sh
@@ -2,7 +2,7 @@
 
 echo "***** INSTALLING COMMON CARGO PACKAGES *****"
 
-cargo install wasm-bindgen-cli taplo-cli cargo-make wasm-pack
+cargo install wasm-bindgen-cli taplo-cli cargo-make wasm-pack cargo-chef cargo-audit cargo-tarpaulin --locked
 
 # this below failed
 # cargo install cargo-audit cargo-tarpaulin
