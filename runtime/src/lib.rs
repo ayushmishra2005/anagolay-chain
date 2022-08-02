8ff1bf9d4d6e4a40f9c46691a5c6d70d2662f6c1
adriano.dalpane <adriano.dalpane@gmail.com>
2022-08-02 16:05:00 +0200

    prepare runtime upgrade

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -129,7 +129,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
   //   `spec_version`, and `authoring_version` are the same between Wasm and native.
   // This value is set to 100 to notify Polkadot-JS App (https://polkadot.js.org/apps) to use
   //   the compatible custom types.
-  spec_version: 100,
+  // It must be incremented every time a new chain spec is generated
+  spec_version: 101,
   impl_version: 1,
   apis: RUNTIME_API_VERSIONS,
   transaction_version: 1,
